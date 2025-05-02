63f9758fff2ca54528aec26207ef8227cf00b97c
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-02 19:52:15 -0300

    Fiz o 4, que era pra fazer essa aplicação que removia os itens pedidos dentro de uma array, sendo strings ou números, e também fazer o que os outros testes pediam.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...otherAruments) {
+    let result = []
+    let shouldRemove
+    
+    for (i = 0; i < array.length; i++) {
+        shouldRemove = false
+
+        for (j = 0; j < otherAruments.length; j++) {
+            if (array[i] === otherAruments[j]) {
+                shouldRemove = true
+                break
+            }
+        }
+
+        if (!shouldRemove) {
+            result.push(array[i])
+        }
+    }
+
+    return result
 };
 
 // Do not edit below this line
