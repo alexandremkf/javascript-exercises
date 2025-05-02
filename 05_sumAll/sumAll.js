563f4a9364877fd778f626b756079061126da528
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-02 20:12:50 -0300

    Fiz o 5, que era pra fazer a soma das números entre um range que fosse passado como argumento, e também passar nos teste que pedia.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0
 
+    if (min > max) {
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+
+    if (min < 0 || max < 0) {
+        return 'ERROR'
+    }
+
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+    
+    for (i = min; i <= max; i++) {
+        sum += i
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
