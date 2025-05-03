000bf6cd4d8f55ab7a8ed5b8c3f08f9f49bdbae7
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-02 21:20:03 -0300

    Fiz o 6, que era pra ver se o ano era bissexto, e passar nos testes que pede.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const divByFourHundred = year % 400 === 0;
 
+    if (divByFour && (!isCentury || divByFourHundred)) {
+        return true;
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
